d4382ecf106ba4be8da3e573f124b93719b66fc2
MrNNP <reachnp64@gmail.com>
2024-12-08 20:50:29 -0500

    Files

---
+++ src/lib/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let path: string;
+    export let classData: string;
+    export let color: string;
+</script>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div on:click={()=>fetch("http://group27Fan.resource.campus.njit.edu/"+path.toLowerCase()+"button", {
+    mode: "no-cors",
+})}
+class={classData + " " + color}
+>{path}</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Button from "$lib/button.svelte";
+    const classData = "p-2 py-20 m-1 rounded-3xl shadow-lg h-max";
+</script>
+<div class="grid grid-rows-3 grid-cols-2  justify-center items-stretch justify-items-stretch p-11 h-max text-white text-3xl md:text-9xl text-center w-full rounded-lg">
+    <span hidden class=""/>
+    <Button classData={classData+" col-span-2"} path="Power" color="bg-black"/> 
+    <Button classData={classData} path="Speed" color="bg-black"/>
+    <Button classData={classData} path="Rotation" color="bg-black"/>
+    <Button classData={classData} path="Sleep" color="bg-black"/>
+    <Button classData={classData} path="Timer" color="bg-black"/>
+    
+</div>
+<!-- <img src="/0001-0097.gif" class=""/> -->
